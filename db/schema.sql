d76995231d5fb22de10ceb9fb7a846f964728686
Matt11711 <matthew.d.allen97@gmail.com>
2022-05-01 00:02:40 -0400

    set up database

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,32 @@
-DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS employees;
 
-DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee_roles;
 
-DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS departments;
 
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-)
+    department_name VARCHAR(30) NOT NULL
+);
 
+CREATE TABLE employee_roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INTEGER,
+CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER DEFAULT NULL REFERENCES employees(id) ON DELETE SET NULL,
+CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES employee_roles(id) 
+-- CONSTRAINT (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
+
+-- my query for whenever I need it
+-- Select a.*, b.first_name FROM employees a LEFT JOIN employees b ON a.manager_id = b.id;
