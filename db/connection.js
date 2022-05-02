ad160b75a1af7a92ed3edd9ed372b146298462e1
Matt11711 <matthew.d.allen97@gmail.com>
2022-05-02 14:34:33 -0400

    all db calls finished(i hope)

--- db/connection.js
+++ db/connection.js
@@ -8,6 +8,7 @@ const db = mysql.createConnection(
       user: process.env.DB_USER,
       // Your MySQL password
       password: process.env.DB_PASSWORD,
+      // your database name
       database: process.env.DB_NAME,
     },
     console.log("Connected to the election database.")
--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,9 @@ DROP TABLE IF EXISTS employee_roles;
 
 DROP TABLE IF EXISTS departments;
 
-
+-- pretty straightforward file, this sets up the database so each of these create statements is a separate table
+-- that has specific fields and requirements. Important notes are that employees references itself and employee_roles
+-- and employee_roles references departments
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL UNIQUE
@@ -25,7 +27,6 @@ CREATE TABLE employees (
 role_id INTEGER NOT NULL,
 manager_id INTEGER DEFAULT NULL REFERENCES employees(id) ON DELETE SET NULL,
 CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES employee_roles(id) 
--- CONSTRAINT (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
 
 -- my query for whenever I need it
