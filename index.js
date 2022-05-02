08b6f7415ae98f35e2f4a3883439af96040eb223
Matt11711 <matthew.d.allen97@gmail.com>
2022-05-02 15:18:28 -0400

    first two inquirer prompts working

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 const db = require('./db/connection');
 const inquirer = require("inquirer")
-const Department = require("./lib/department")
+const Department = require("./lib/department");
+const Role = require("./lib/role")
+const Employee = require("./lib/employee")
+const ExpandPrompt = require('inquirer/lib/prompts/expand');
 const department = new Department
+const role = new Role
+const employee = new Employee
 
 
-// new Department("testing").post()
-
-const test = () => {
+// prompt for user to choose their actions
+const prompt = async () => {
  inquirer.prompt(
         {
           type: "list",
           name: "choice",
           message: "What would you like to do?",
-          choices: ["See Departments", "Intern", "No one else"],
-        }).then((choice) => {
-            if (choice.choice==="See Departments") {
+          choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role","quit"],
+        }).then(async (choice) => {
+            if (choice.choice==="view all departments") {
+                // show the departments then prompt again
+                 department.get()
+                prompt()
+            }
+            if (choice.choice==="view all roles") {
+                role.get()
+            }
+            if (choice.choice==="view all employees") {
+                const employeesTable = await employee.get
+                console.table(employeesTable)
+                prompt()
+            }
+            if (choice.choice==="add a department") {
                 department.get()
             }
+            if (choice.choice==="add a role") {
+                department.get()
+            }
+            if (choice.choice==="add an employee") {
+             addEmployee()
+            }
+            if (choice.choice==="update an employee role") {
+                department.get()
+            }
+            if (choice.choice==="quit") {
+                console.log("Ending app instance.");
+                process.kill(process.pid, 'SIGTERM');
+            }
+
         }) }
 
 // department.get()
 
-test();
+function addEmployee() {
+    
+}
+
+prompt();
 
 
 
